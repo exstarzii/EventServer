c51f7e7813bf20017f5ddb54ae76fa3d554a676d
exstarzii <exstarzii@yandex.ru>
2023-05-11 00:11:39 +0300

    damn fix part 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,35 @@ import { Response } from 'express';
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import { MongoError } from 'mongodb';
 
+@Catch(MongoError)
+export class MongoExceptionFilter implements ExceptionFilter {
+  catch(exception: MongoError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =  HttpStatus.BAD_REQUEST;
+    let message = 'mongo exception'
+    const start = exception.message.indexOf('{')
+    const end = exception.message.indexOf('}',start)+1
+    console.log(exception.message)
+    switch (exception.code) {
+      case 11000:
+        // duplicate exception
+        // do whatever you want here, for instance send error to client
+        
+        message = 'duplicate '+exception.message.substring(start,end)
+    }
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: [message]
+      });
+  }
+}
+
 @Controller('user')
 export class AuthController {
     constructor(private authService: AuthService) {}
@@ -87,35 +116,5 @@ export class AuthController {
   @Delete()
   async remove(@Request() req) {
     return this.authService.deleteUser(req.user.userId);
-  }
-}
-
-
-@Catch(MongoError)
-export class MongoExceptionFilter implements ExceptionFilter {
-  catch(exception: MongoError, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status =  HttpStatus.BAD_REQUEST;
-    let message = 'mongo exception'
-    const start = exception.message.indexOf('{')
-    const end = exception.message.indexOf('}',start)+1
-    console.log(exception.message)
-    switch (exception.code) {
-      case 11000:
-        // duplicate exception
-        // do whatever you want here, for instance send error to client
-        
-        message = 'duplicate '+exception.message.substring(start,end)
-    }
-    response
-      .status(status)
-      .json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-        message: [message]
-      });
   }
 }
