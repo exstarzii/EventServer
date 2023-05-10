537285c5e2adcac7007e75ea47f04542b6b7c961
exstarzii <exstarzii@yandex.ru>
2023-05-11 00:08:30 +0300

    damn fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,9 @@ import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { User } from '../schemas/user.schema';
-import { MongoExceptionFilter } from 'src/httpexception.filter';
+import { Response } from 'express';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import { MongoError } from 'mongodb';
 
 @Controller('user')
 export class AuthController {
@@ -85,5 +87,35 @@ export class AuthController {
   @Delete()
   async remove(@Request() req) {
     return this.authService.deleteUser(req.user.userId);
+  }
+}
+
+
+@Catch(MongoError)
+export class MongoExceptionFilter implements ExceptionFilter {
+  catch(exception: MongoError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =  HttpStatus.BAD_REQUEST;
+    let message = 'mongo exception'
+    const start = exception.message.indexOf('{')
+    const end = exception.message.indexOf('}',start)+1
+    console.log(exception.message)
+    switch (exception.code) {
+      case 11000:
+        // duplicate exception
+        // do whatever you want here, for instance send error to client
+        
+        message = 'duplicate '+exception.message.substring(start,end)
+    }
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: [message]
+      });
   }
 }
